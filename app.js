39d27e423a9dfd20237cee9a5cb3144465154c24
Kyle Jacobs <kycjacob@gmail.com>
2022-04-20 14:42:04 -0700

    Made functioning buttons with small animations, added a random colour mode

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const SIZE_DEFAULT = 16;
 const COLOR_DEFAULT = "black"; // Maybe delete? Not really used.
 const container = document.getElementById('container');
+const nat_colors = ["#d5ce3b", "#aa7b08", "#9ab106", "#385802", "#3e3e07", 
+                    "#2c2342", "#555515", "#b0bc24", "#e09f3e", "#fff3b0",
+                    "#9e2a2b", "#540b0e"];
+let NAT_FLAG = false;
 let SIZE_NEW = SIZE_DEFAULT;
 let COLOR_NEW = COLOR_DEFAULT;
 let colorInput = document.querySelector('#color');
@@ -10,17 +14,18 @@ colorInput.addEventListener('input', () => {
 });
 
 function getNewSize(){
+    
     let currentSize = SIZE_NEW;
-    SIZE_NEW = prompt("Enter desired size for new grid (between 1 and 100): ");
+    SIZE_NEW = prompt("Enter desired size for new grid (between 1 and 128): ");
+    
     if (SIZE_NEW == null) {
         SIZE_NEW = currentSize;
         return;
     }
-    else if(SIZE_NEW < 1 || SIZE_NEW > 100) {
+    else if(SIZE_NEW < 1 || SIZE_NEW > 128) {
         alert("Invalid Size");
         getNewSize();
     }
-    
 }
 
 function createGrid(size) {
@@ -52,7 +57,6 @@ function deleteGrid(){
 function clearGrid(){
     deleteGrid();
     createGrid(SIZE_NEW);
-    toggleGridLines();
 }
 
 function toggleGridLines(){
@@ -88,9 +92,41 @@ function stopDrawing() {
 }
 
 function draw(e) {
+    
+    if(NAT_FLAG) {
+        e.target.style.backgroundColor = nat_colors[randomInt(0, 12)];
+        return;
+    }
+
     e.target.style.backgroundColor = COLOR_NEW;
+}
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function natColorToggle() {
+    if(NAT_FLAG) {
+        NAT_FLAG = false;
+    }
+    else {
+        NAT_FLAG = true;
+    }
+}
+
+function natColorEvent() {
+    const btn = document.getElementById('nature-colors');
+    btn.addEventListener('click', function onClick() {
+        if(NAT_FLAG) {
+            btn.style.backgroundColor = "#855e42";
+        }
+        else {
+            btn.style.backgroundColor = "#cfe1c9";
+        }
+    });
 }
 
 window.onload = () => {
     createGrid(SIZE_DEFAULT);
+    natColorEvent();
 }
